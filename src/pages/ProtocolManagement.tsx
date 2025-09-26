102557755775e026679d2f3a1812e6bbb5a69ff3
jifan <jifanchn@gmail.com>
2025-09-26 09:53:01 +0800

    [dyad] wrote 2 file(s)

--- src/pages/ProtocolManagement.tsx
+++ src/pages/ProtocolManagement.tsx
@@ -38,43 +38,46 @@ const ProtocolManagement = () => {
       </div>
       
       <div className="container mx-auto py-6">
-        <Tabs value={activeTab} onValueChange={setActiveTab} className="mb-6">
-          <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger value="device-types" className="py-2">
-              <Package className="h-4 w-4 mr-2" />
-              设备类型
-            </TabsTrigger>
-            <TabsTrigger value="scan-config" className="py-2">
-              <Scan className="h-4 w-4 mr-2" />
-              寄存器扫描
-            </TabsTrigger>
-            <TabsTrigger value="register-table" className="py-2">
-              <Database className="h-4 w-4 mr-2" />
-              寄存器表
-            </TabsTrigger>
-            <TabsTrigger value="probe-config" className="py-2">
-              <Eye className="h-4 w-4 mr-2" />
-              Probe条件
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
-
         <Card className="w-full">
-          <TabsContent value="device-types" className="p-0">
-            <DeviceTypeManagement />
-          </TabsContent>
-          
-          <TabsContent value="scan-config" className="p-0">
-            <RegisterScanConfig />
-          </TabsContent>
-          
-          <TabsContent value="register-table" className="p-0">
-            <DeviceRegisterTable />
-          </TabsContent>
-          
-          <TabsContent value="probe-config" className="p-0">
-            <DeviceProbeConfig />
-          </TabsContent>
+          <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
+            <CardHeader>
+              <TabsList className="grid w-full grid-cols-4">
+                <TabsTrigger value="device-types" className="py-2">
+                  <Package className="h-4 w-4 mr-2" />
+                  设备类型
+                </TabsTrigger>
+                <TabsTrigger value="scan-config" className="py-2">
+                  <Scan className="h-4 w-4 mr-2" />
+                  寄存器扫描
+                </TabsTrigger>
+                <TabsTrigger value="register-table" className="py-2">
+                  <Database className="h-4 w-4 mr-2" />
+                  寄存器表
+                </TabsTrigger>
+                <TabsTrigger value="probe-config" className="py-2">
+                  <Eye className="h-4 w-4 mr-2" />
+                  Probe条件
+                </TabsTrigger>
+              </TabsList>
+            </CardHeader>
+            <CardContent>
+              <TabsContent value="device-types" className="mt-0">
+                <DeviceTypeManagement />
+              </TabsContent>
+              
+              <TabsContent value="scan-config" className="mt-0">
+                <RegisterScanConfig />
+              </TabsContent>
+              
+              <TabsContent value="register-table" className="mt-0">
+                <DeviceRegisterTable />
+              </TabsContent>
+              
+              <TabsContent value="probe-config" className="mt-0">
+                <DeviceProbeConfig />
+              </TabsContent>
+            </CardContent>
+          </Tabs>
         </Card>
       </div>
       
